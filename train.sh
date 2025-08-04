1d992116191269a6db719dcf133a643482ae6cde
farzad-bz <beizaeefarzad@gmail.com>
2025-08-04 16:14:47 -0400

    Update codes

--- train.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 torchrun evaluate_REFLECT.py \
-            --data-dir /home-2/ar94660/Datasets/Medical/Brats2023-slices/ \
-            #model path
-            --model-path /home-2/ar94660/REFLECT/REFLECT_BraTS_UNet_S_T1_256_kl_f8/000-UNet_S-T1/checkpoints/last.pt \
